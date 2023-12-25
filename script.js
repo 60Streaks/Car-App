e50c7aa661a00a28995016da6c8435afcd954fc7
60Streaks <151665741+60Streaks@users.noreply.github.com>
2023-12-25 22:39:06 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function playMusic() {
+    // Implement logic to play music
+    document.getElementById('result').innerHTML = 'Music is playing.';
+}
+
+function pauseMusic() {
+    // Implement logic to pause music
+    document.getElementById('result').innerHTML = 'Music is paused.';
+}
+
+function showVehicleInfo() {
+    // Implement logic to display vehicle information
+    document.getElementById('result').innerHTML = 'Vehicle Information:\nModel: XYZ\nYear: 2022\nFuel: Gasoline';
+}
